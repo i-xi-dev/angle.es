f401c9493cda6e94e563491d9acff11e1416edd4
i-xi-dev <naoyuki.isono@gmail.com>
2024-05-04 22:59:16 +0900

    2.0.0

--- src/angle.ts
+++ src/angle.ts
@@ -1,10 +1,10 @@
 import { RoundingMode, SafeInteger } from "../deps.ts";
 
-class Angle {
+export class Angle {
   #degrees: Angle.Degrees;
 
   private constructor(degrees: Angle.Degrees) {
-    this.#degrees = Angle.Degrees.normalize(degrees);
+    this.#degrees = Angle.normalizeDegrees(degrees);
     Object.freeze(this);
   }
 
@@ -13,7 +13,7 @@ class Angle {
   }
 
   static ofRadians(radians: Angle.Radians): Angle {
-    return new Angle(Angle.Degrees.fromRadians(radians));
+    return new Angle(Angle.radiansToDegrees(radians));
   }
 
   toDegrees(): Angle.Degrees {
@@ -21,150 +21,149 @@ class Angle {
   }
 
   toRadians(): Angle.Radians {
-    return Angle.Degrees.toRadians(this.#degrees);
+    return Angle.degreesToRadians(this.#degrees);
+  }
+
+  toDmsString(options?: Angle.DmsOptions): string { //TODO test
+    return Angle.degreesToDmsString(this.#degrees, options);
   }
 
   valueOf(): number {
-    return this.toDegrees();
+    return this.#degrees;
   }
 }
 
-namespace Angle {
+export namespace Angle {
   export type Degrees = number;
 
   export namespace Degrees {
-    const ZERO_TURN = 0;
-    const ONE_TURN = 360;
-
-    export function normalize(degrees: Degrees): Degrees {
-      if (Number.isFinite(degrees)) {
-        const t = degrees % ONE_TURN;
-        return (t < ZERO_TURN) ? (t + ONE_TURN) : t;
-      }
-      throw new TypeError("degrees");
-    }
-
-    export function fromRadians(radians: Radians): Degrees {
-      if (Number.isFinite(radians)) {
-        const degrees = radians * (180 / Math.PI);
-        return normalize(degrees);
-      }
-      throw new TypeError("radians");
-    }
-
-    export function toRadians(degrees: Degrees): Radians {
-      if (Number.isFinite(degrees)) {
-        return normalize(degrees) * (Math.PI / 180);
-      }
-      throw new TypeError("degrees");
-    }
-
-    export function fromGradians(gradians: Gradians): Degrees {
-      if (Number.isFinite(gradians)) {
-        const degrees = gradians * (180 / 200);
-        return normalize(degrees);
-      }
-      throw new TypeError("gradians");
-    }
-
-    export function fromTurns(turns: Turns): Degrees {
-      if (Number.isFinite(turns)) {
-        const degrees = turns * 360;
-        return normalize(degrees);
-      }
-      throw new TypeError("turns");
-    }
-
-    export function toDmsString(
-      degrees: Degrees,
-      options?: DmsOptions,
-    ): string {
-      if (Number.isFinite(degrees) !== true) {
-        throw new TypeError("degrees");
-      }
-
-      const precision = Object.values(DmsOptions.Precision).includes(
-          options?.precision as DmsOptions.Precision,
-        )
-        ? options?.precision
-        : DmsOptions.Precision.SECOND;
-      const normalizedDegrees = normalize(degrees);
-      const dInt = Math.trunc(normalizedDegrees);
-
-      let dStr: string;
-      if (
-        (precision === DmsOptions.Precision.DEGREE) ||
-        ((precision === DmsOptions.Precision.AUTO) &&
-          (dInt === normalizedDegrees))
-      ) {
-        dStr = Math.round(normalizedDegrees).toString(10);
-        return `${dStr}°`;
-      } else {
-        dStr = dInt.toString(10);
-      }
-
-      const msNum = (normalizedDegrees - dInt) * 60;
-      const mInt = Math.trunc(msNum);
-
-      let mStr: string;
-      if (
-        (precision === DmsOptions.Precision.MINUTE) ||
-        ((precision === DmsOptions.Precision.AUTO) && (mInt === msNum))
-      ) {
-        mStr = Math.round(msNum).toString(10).padStart(2, "0");
-        return `${dStr}°${mStr}′`;
-      } else {
-        mStr = mInt.toString(10).padStart(2, "0");
-      }
-
-      const sNum = (msNum - mInt) * 60;
-      const sInt = Math.trunc(sNum);
-
-      const secondFractionDigits = SafeInteger.fromNumber(
-        options?.secondFractionDigits,
-        {
-          fallback: 0,
-          roundingMode: RoundingMode.TRUNCATE,
-          clampRange: [0, 6],
-        },
-      );
-
-      const sStr = ((sInt < 10) ? "0" : "") +
-        sNum.toFixed(secondFractionDigits);
-      return `${dStr}°${mStr}′${sStr}″`;
-    }
-
-    export type DmsOptions = {
-      precision?: DmsOptions.Precision;
-      secondFractionDigits?: DmsOptions.SecondFractionDigits;
-    };
-
-    export namespace DmsOptions {
-      export const Precision = {
-        AUTO: "auto",
-        DEGREE: "degree",
-        MINUTE: "minute",
-        SECOND: "second",
-        // MILLISECOND → SecondFractionDigits:3
-        // MICROSECOND → SecondFractionDigits:6
-      } as const;
-      export type Precision = typeof Precision[keyof typeof Precision];
-
-      export type SecondFractionDigits = 0 | 1 | 2 | 3 | 4 | 5 | 6;
-    }
+    export const ZERO_TURN = 0;
+    export const ONE_TURN = 360;
   }
 
   export type Radians = number;
 
-  // export namespace Radians {
-  //   export function format(radians: Radians): string {
-  //     return `${ radians } rad`; 桁区切りはSI単位としては","でなく" "。→ 基本的には" rad"を後置すればいいだけなので実装しない
-  //   }
-  // }
-
   export type Gradians = number;
 
   export type Turns = number;
-}
 
-export { Angle };
+  export function normalizeDegrees(degrees: Degrees): Degrees {
+    if (Number.isFinite(degrees)) {
+      const t = degrees % Degrees.ONE_TURN;
+      return (t < Degrees.ZERO_TURN) ? (t + Degrees.ONE_TURN) : t;
+    }
+    throw new TypeError("degrees");
+  }
+
+  export function radiansToDegrees(radians: Radians): Degrees {
+    if (Number.isFinite(radians)) {
+      const degrees = radians * (180 / Math.PI);
+      return normalizeDegrees(degrees);
+    }
+    throw new TypeError("radians");
+  }
+
+  export function degreesToRadians(degrees: Degrees): Radians {
+    if (Number.isFinite(degrees)) {
+      return normalizeDegrees(degrees) * (Math.PI / 180);
+    }
+    throw new TypeError("degrees");
+  }
+
+  export function gradiansToDegrees(gradians: Gradians): Degrees {
+    if (Number.isFinite(gradians)) {
+      const degrees = gradians * (180 / 200);
+      return normalizeDegrees(degrees);
+    }
+    throw new TypeError("gradians");
+  }
+
+  //TODO degreesToGradians
+
+  export function turnsToDegrees(turns: Turns): Degrees {
+    if (Number.isFinite(turns)) {
+      const degrees = turns * 360;
+      return normalizeDegrees(degrees);
+    }
+    throw new TypeError("turns");
+  }
+
+  //TODO degreesToTurns
+
+  export const DmsStringPrecision = {
+    AUTO: "auto",
+    DEGREE: "degree",
+    MINUTE: "minute",
+    SECOND: "second",
+    // MILLISECOND → SecondFractionDigits:3
+    // MICROSECOND → SecondFractionDigits:6
+  } as const;
+  export type DmsStringPrecision =
+    typeof DmsStringPrecision[keyof typeof DmsStringPrecision];
+
+  export type DmsStringSecondFractionDigits = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+
+  export type DmsOptions = {
+    precision?: DmsStringPrecision;
+    secondFractionDigits?: DmsStringSecondFractionDigits;
+  };
+
+  export function degreesToDmsString(
+    degrees: Degrees,
+    options?: DmsOptions,
+  ): string {
+    if (Number.isFinite(degrees) !== true) {
+      throw new TypeError("degrees");
+    }
+
+    const precision = Object.values(DmsStringPrecision).includes(
+        options?.precision as DmsStringPrecision,
+      )
+      ? options?.precision
+      : DmsStringPrecision.SECOND;
+    const normalizedDegrees = normalizeDegrees(degrees);
+    const dInt = Math.trunc(normalizedDegrees);
+
+    let dStr: string;
+    if (
+      (precision === DmsStringPrecision.DEGREE) ||
+      ((precision === DmsStringPrecision.AUTO) &&
+        (dInt === normalizedDegrees))
+    ) {
+      dStr = Math.round(normalizedDegrees).toString(10);
+      return `${dStr}°`;
+    } else {
+      dStr = dInt.toString(10);
+    }
+
+    const msNum = (normalizedDegrees - dInt) * 60;
+    const mInt = Math.trunc(msNum);
+
+    let mStr: string;
+    if (
+      (precision === DmsStringPrecision.MINUTE) ||
+      ((precision === DmsStringPrecision.AUTO) && (mInt === msNum))
+    ) {
+      mStr = Math.round(msNum).toString(10).padStart(2, "0");
+      return `${dStr}°${mStr}′`;
+    } else {
+      mStr = mInt.toString(10).padStart(2, "0");
+    }
+
+    const sNum = (msNum - mInt) * 60;
+    const sInt = Math.trunc(sNum);
+
+    const secondFractionDigits = SafeInteger.fromNumber(
+      options?.secondFractionDigits,
+      {
+        fallback: 0,
+        roundingMode: RoundingMode.TRUNCATE,
+        clampRange: [0, 6],
+      },
+    );
+
+    const sStr = ((sInt < 10) ? "0" : "") +
+      sNum.toFixed(secondFractionDigits);
+    return `${dStr}°${mStr}′${sStr}″`;
+  }
+}
--- tests/angle_degree.test.ts
+++ tests/angle_degree.test.ts
@@ -1,22 +1,22 @@
 import { assertStrictEquals, assertThrows } from "./deps.ts";
 import { Angle } from "../mod.ts";
 
-Deno.test("Angle.Degrees.normalize()", () => {
-  assertStrictEquals(Angle.Degrees.normalize(0), 0);
-  assertStrictEquals(Angle.Degrees.normalize(360), 0);
-  assertStrictEquals(Angle.Degrees.normalize(1), 1);
-  assertStrictEquals(Angle.Degrees.normalize(0.1), 0.1);
-  assertStrictEquals(Angle.Degrees.normalize(359), 359);
-  assertStrictEquals(Angle.Degrees.normalize(359.9), 359.9);
+Deno.test("Angle.normalizeDegrees()", () => {
+  assertStrictEquals(Angle.normalizeDegrees(0), 0);
+  assertStrictEquals(Angle.normalizeDegrees(360), 0);
+  assertStrictEquals(Angle.normalizeDegrees(1), 1);
+  assertStrictEquals(Angle.normalizeDegrees(0.1), 0.1);
+  assertStrictEquals(Angle.normalizeDegrees(359), 359);
+  assertStrictEquals(Angle.normalizeDegrees(359.9), 359.9);
   assertStrictEquals(
-    Angle.Degrees.normalize(360.1).toFixed(6),
+    Angle.normalizeDegrees(360.1).toFixed(6),
     (0.1).toFixed(6),
   ); // JSの精度の問題
-  assertStrictEquals(Angle.Degrees.normalize(-0.1), 359.9);
+  assertStrictEquals(Angle.normalizeDegrees(-0.1), 359.9);
 
   assertThrows(
     () => {
-      Angle.Degrees.normalize("0" as unknown as number);
+      Angle.normalizeDegrees("0" as unknown as number);
     },
     TypeError,
     undefined,
@@ -24,19 +24,19 @@ Deno.test("Angle.Degrees.normalize()", () => {
   );
 });
 
-Deno.test("Angle.Degrees.fromRadians()", () => {
-  assertStrictEquals(Angle.Degrees.fromRadians(0), 0);
-  assertStrictEquals(Angle.Degrees.fromRadians(Math.PI * 2), 0);
-  assertStrictEquals(Angle.Degrees.fromRadians(Math.PI), 180);
-  assertStrictEquals(Angle.Degrees.fromRadians(Math.PI / 2), 90);
+Deno.test("Angle.radiansToDegrees()", () => {
+  assertStrictEquals(Angle.radiansToDegrees(0), 0);
+  assertStrictEquals(Angle.radiansToDegrees(Math.PI * 2), 0);
+  assertStrictEquals(Angle.radiansToDegrees(Math.PI), 180);
+  assertStrictEquals(Angle.radiansToDegrees(Math.PI / 2), 90);
   assertStrictEquals(
-    Angle.Degrees.fromRadians((Math.PI * 2) + (Math.PI / 2)),
+    Angle.radiansToDegrees((Math.PI * 2) + (Math.PI / 2)),
     90,
   );
 
   assertThrows(
     () => {
-      Angle.Degrees.fromRadians("0" as unknown as number);
+      Angle.radiansToDegrees("0" as unknown as number);
     },
     TypeError,
     undefined,
@@ -44,16 +44,16 @@ Deno.test("Angle.Degrees.fromRadians()", () => {
   );
 });
 
-Deno.test("Angle.Degrees.toRadians()", () => {
-  assertStrictEquals(Angle.Degrees.toRadians(0), 0);
-  assertStrictEquals(Angle.Degrees.toRadians(360), 0);
-  assertStrictEquals(Angle.Degrees.toRadians(180), Math.PI);
-  assertStrictEquals(Angle.Degrees.toRadians(90), Math.PI / 2);
-  assertStrictEquals(Angle.Degrees.toRadians(450), Math.PI / 2);
+Deno.test("Angle.degreesToRadians()", () => {
+  assertStrictEquals(Angle.degreesToRadians(0), 0);
+  assertStrictEquals(Angle.degreesToRadians(360), 0);
+  assertStrictEquals(Angle.degreesToRadians(180), Math.PI);
+  assertStrictEquals(Angle.degreesToRadians(90), Math.PI / 2);
+  assertStrictEquals(Angle.degreesToRadians(450), Math.PI / 2);
 
   assertThrows(
     () => {
-      Angle.Degrees.toRadians("0" as unknown as number);
+      Angle.degreesToRadians("0" as unknown as number);
     },
     TypeError,
     undefined,
@@ -61,19 +61,19 @@ Deno.test("Angle.Degrees.toRadians()", () => {
   );
 });
 
-Deno.test("Angle.Degrees.toDmsString(number)", () => {
-  assertStrictEquals(Angle.Degrees.toDmsString(0), "0°00′00″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1234567890), "90°00′00″");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.1), "0°06′00″");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.5), "0°30′00″");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.9), "0°54′00″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23), "1°13′48″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.234), "1°14′02″");
-  assertStrictEquals(Angle.Degrees.toDmsString(6.8563), "6°51′23″");
+Deno.test("Angle.degreesToDmsString(number)", () => {
+  assertStrictEquals(Angle.degreesToDmsString(0), "0°00′00″");
+  assertStrictEquals(Angle.degreesToDmsString(1234567890), "90°00′00″");
+  assertStrictEquals(Angle.degreesToDmsString(0.1), "0°06′00″");
+  assertStrictEquals(Angle.degreesToDmsString(0.5), "0°30′00″");
+  assertStrictEquals(Angle.degreesToDmsString(0.9), "0°54′00″");
+  assertStrictEquals(Angle.degreesToDmsString(1.23), "1°13′48″");
+  assertStrictEquals(Angle.degreesToDmsString(1.234), "1°14′02″");
+  assertStrictEquals(Angle.degreesToDmsString(6.8563), "6°51′23″");
 
   assertThrows(
     () => {
-      Angle.Degrees.toDmsString("0" as unknown as number);
+      Angle.degreesToDmsString("0" as unknown as number);
     },
     TypeError,
     undefined,
@@ -81,129 +81,129 @@ Deno.test("Angle.Degrees.toDmsString(number)", () => {
   );
 });
 
-Deno.test("Angle.Degrees.toDmsString(number, {})", () => {
+Deno.test("Angle.degreesToDmsString(number, {})", () => {
   const o1 = { secondFractionDigits: 0, precision: "auto" } as const;
-  assertStrictEquals(Angle.Degrees.toDmsString(0, o1), "0°");
-  assertStrictEquals(Angle.Degrees.toDmsString(1234567890, o1), "90°");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.1, o1), "0°06′");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.5, o1), "0°30′");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.9, o1), "0°54′");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23, o1), "1°13′48″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.234, o1), "1°14′02″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23456, o1), "1°14′04″");
-  assertStrictEquals(Angle.Degrees.toDmsString(6.8563, o1), "6°51′23″");
+  assertStrictEquals(Angle.degreesToDmsString(0, o1), "0°");
+  assertStrictEquals(Angle.degreesToDmsString(1234567890, o1), "90°");
+  assertStrictEquals(Angle.degreesToDmsString(0.1, o1), "0°06′");
+  assertStrictEquals(Angle.degreesToDmsString(0.5, o1), "0°30′");
+  assertStrictEquals(Angle.degreesToDmsString(0.9, o1), "0°54′");
+  assertStrictEquals(Angle.degreesToDmsString(1.23, o1), "1°13′48″");
+  assertStrictEquals(Angle.degreesToDmsString(1.234, o1), "1°14′02″");
+  assertStrictEquals(Angle.degreesToDmsString(1.23456, o1), "1°14′04″");
+  assertStrictEquals(Angle.degreesToDmsString(6.8563, o1), "6°51′23″");
 
   const o2 = {
-    secondFractionDigits: 10 as Angle.Degrees.DmsOptions.SecondFractionDigits,
+    secondFractionDigits: 10 as Angle.DmsStringSecondFractionDigits,
     precision: "auto",
   } as const;
-  assertStrictEquals(Angle.Degrees.toDmsString(0, o2), "0°");
-  assertStrictEquals(Angle.Degrees.toDmsString(1234567890, o2), "90°");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.1, o2), "0°06′");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.5, o2), "0°30′");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.9, o2), "0°54′");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23, o2), "1°13′48.000000″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.234, o2), "1°14′02.400000″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23456, o2), "1°14′04.416000″");
-  assertStrictEquals(Angle.Degrees.toDmsString(6.8563, o2), "6°51′22.680000″");
+  assertStrictEquals(Angle.degreesToDmsString(0, o2), "0°");
+  assertStrictEquals(Angle.degreesToDmsString(1234567890, o2), "90°");
+  assertStrictEquals(Angle.degreesToDmsString(0.1, o2), "0°06′");
+  assertStrictEquals(Angle.degreesToDmsString(0.5, o2), "0°30′");
+  assertStrictEquals(Angle.degreesToDmsString(0.9, o2), "0°54′");
+  assertStrictEquals(Angle.degreesToDmsString(1.23, o2), "1°13′48.000000″");
+  assertStrictEquals(Angle.degreesToDmsString(1.234, o2), "1°14′02.400000″");
+  assertStrictEquals(Angle.degreesToDmsString(1.23456, o2), "1°14′04.416000″");
+  assertStrictEquals(Angle.degreesToDmsString(6.8563, o2), "6°51′22.680000″");
 
   const o3 = { secondFractionDigits: 1, precision: "auto" } as const;
-  assertStrictEquals(Angle.Degrees.toDmsString(0, o3), "0°");
-  assertStrictEquals(Angle.Degrees.toDmsString(1234567890, o3), "90°");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.1, o3), "0°06′");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.5, o3), "0°30′");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.9, o3), "0°54′");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23, o3), "1°13′48.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.234, o3), "1°14′02.4″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23456, o3), "1°14′04.4″");
-  assertStrictEquals(Angle.Degrees.toDmsString(6.8563, o3), "6°51′22.7″");
+  assertStrictEquals(Angle.degreesToDmsString(0, o3), "0°");
+  assertStrictEquals(Angle.degreesToDmsString(1234567890, o3), "90°");
+  assertStrictEquals(Angle.degreesToDmsString(0.1, o3), "0°06′");
+  assertStrictEquals(Angle.degreesToDmsString(0.5, o3), "0°30′");
+  assertStrictEquals(Angle.degreesToDmsString(0.9, o3), "0°54′");
+  assertStrictEquals(Angle.degreesToDmsString(1.23, o3), "1°13′48.0″");
+  assertStrictEquals(Angle.degreesToDmsString(1.234, o3), "1°14′02.4″");
+  assertStrictEquals(Angle.degreesToDmsString(1.23456, o3), "1°14′04.4″");
+  assertStrictEquals(Angle.degreesToDmsString(6.8563, o3), "6°51′22.7″");
 
   const o3x = {
     secondFractionDigits: 1,
-    precision: "" as Angle.Degrees.DmsOptions.Precision,
+    precision: "" as Angle.DmsStringPrecision,
   } as const;
-  assertStrictEquals(Angle.Degrees.toDmsString(0, o3x), "0°00′00.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1234567890, o3x), "90°00′00.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.1, o3x), "0°06′00.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.5, o3x), "0°30′00.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.9, o3x), "0°54′00.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23, o3x), "1°13′48.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.234, o3x), "1°14′02.4″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23456, o3x), "1°14′04.4″");
-  assertStrictEquals(Angle.Degrees.toDmsString(6.8563, o3x), "6°51′22.7″");
+  assertStrictEquals(Angle.degreesToDmsString(0, o3x), "0°00′00.0″");
+  assertStrictEquals(Angle.degreesToDmsString(1234567890, o3x), "90°00′00.0″");
+  assertStrictEquals(Angle.degreesToDmsString(0.1, o3x), "0°06′00.0″");
+  assertStrictEquals(Angle.degreesToDmsString(0.5, o3x), "0°30′00.0″");
+  assertStrictEquals(Angle.degreesToDmsString(0.9, o3x), "0°54′00.0″");
+  assertStrictEquals(Angle.degreesToDmsString(1.23, o3x), "1°13′48.0″");
+  assertStrictEquals(Angle.degreesToDmsString(1.234, o3x), "1°14′02.4″");
+  assertStrictEquals(Angle.degreesToDmsString(1.23456, o3x), "1°14′04.4″");
+  assertStrictEquals(Angle.degreesToDmsString(6.8563, o3x), "6°51′22.7″");
 
   const o3b = { secondFractionDigits: 1, precision: "auto" } as const;
-  assertStrictEquals(Angle.Degrees.toDmsString(0, o3b), "0°");
-  assertStrictEquals(Angle.Degrees.toDmsString(1234567890, o3b), "90°");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.1, o3b), "0°06′");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.5, o3b), "0°30′");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.9, o3b), "0°54′");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23, o3b), "1°13′48.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.234, o3b), "1°14′02.4″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23456, o3b), "1°14′04.4″");
-  assertStrictEquals(Angle.Degrees.toDmsString(6.8563, o3b), "6°51′22.7″");
+  assertStrictEquals(Angle.degreesToDmsString(0, o3b), "0°");
+  assertStrictEquals(Angle.degreesToDmsString(1234567890, o3b), "90°");
+  assertStrictEquals(Angle.degreesToDmsString(0.1, o3b), "0°06′");
+  assertStrictEquals(Angle.degreesToDmsString(0.5, o3b), "0°30′");
+  assertStrictEquals(Angle.degreesToDmsString(0.9, o3b), "0°54′");
+  assertStrictEquals(Angle.degreesToDmsString(1.23, o3b), "1°13′48.0″");
+  assertStrictEquals(Angle.degreesToDmsString(1.234, o3b), "1°14′02.4″");
+  assertStrictEquals(Angle.degreesToDmsString(1.23456, o3b), "1°14′04.4″");
+  assertStrictEquals(Angle.degreesToDmsString(6.8563, o3b), "6°51′22.7″");
 
   const o3c = { secondFractionDigits: 1, precision: "second" } as const;
-  assertStrictEquals(Angle.Degrees.toDmsString(0, o3c), "0°00′00.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1234567890, o3c), "90°00′00.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.1, o3c), "0°06′00.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.5, o3c), "0°30′00.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.9, o3c), "0°54′00.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23, o3c), "1°13′48.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.234, o3c), "1°14′02.4″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23456, o3c), "1°14′04.4″");
-  assertStrictEquals(Angle.Degrees.toDmsString(6.8563, o3c), "6°51′22.7″");
+  assertStrictEquals(Angle.degreesToDmsString(0, o3c), "0°00′00.0″");
+  assertStrictEquals(Angle.degreesToDmsString(1234567890, o3c), "90°00′00.0″");
+  assertStrictEquals(Angle.degreesToDmsString(0.1, o3c), "0°06′00.0″");
+  assertStrictEquals(Angle.degreesToDmsString(0.5, o3c), "0°30′00.0″");
+  assertStrictEquals(Angle.degreesToDmsString(0.9, o3c), "0°54′00.0″");
+  assertStrictEquals(Angle.degreesToDmsString(1.23, o3c), "1°13′48.0″");
+  assertStrictEquals(Angle.degreesToDmsString(1.234, o3c), "1°14′02.4″");
+  assertStrictEquals(Angle.degreesToDmsString(1.23456, o3c), "1°14′04.4″");
+  assertStrictEquals(Angle.degreesToDmsString(6.8563, o3c), "6°51′22.7″");
 
   const o3cx = { secondFractionDigits: 1 } as const;
-  assertStrictEquals(Angle.Degrees.toDmsString(0, o3cx), "0°00′00.0″");
+  assertStrictEquals(Angle.degreesToDmsString(0, o3cx), "0°00′00.0″");
   assertStrictEquals(
-    Angle.Degrees.toDmsString(1234567890, o3cx),
+    Angle.degreesToDmsString(1234567890, o3cx),
     "90°00′00.0″",
   );
-  assertStrictEquals(Angle.Degrees.toDmsString(0.1, o3cx), "0°06′00.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.5, o3cx), "0°30′00.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.9, o3cx), "0°54′00.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23, o3cx), "1°13′48.0″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.234, o3cx), "1°14′02.4″");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23456, o3cx), "1°14′04.4″");
-  assertStrictEquals(Angle.Degrees.toDmsString(6.8563, o3cx), "6°51′22.7″");
+  assertStrictEquals(Angle.degreesToDmsString(0.1, o3cx), "0°06′00.0″");
+  assertStrictEquals(Angle.degreesToDmsString(0.5, o3cx), "0°30′00.0″");
+  assertStrictEquals(Angle.degreesToDmsString(0.9, o3cx), "0°54′00.0″");
+  assertStrictEquals(Angle.degreesToDmsString(1.23, o3cx), "1°13′48.0″");
+  assertStrictEquals(Angle.degreesToDmsString(1.234, o3cx), "1°14′02.4″");
+  assertStrictEquals(Angle.degreesToDmsString(1.23456, o3cx), "1°14′04.4″");
+  assertStrictEquals(Angle.degreesToDmsString(6.8563, o3cx), "6°51′22.7″");
 
   const o3d = { secondFractionDigits: 1, precision: "minute" } as const;
-  assertStrictEquals(Angle.Degrees.toDmsString(0, o3d), "0°00′");
-  assertStrictEquals(Angle.Degrees.toDmsString(1234567890, o3d), "90°00′");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.1, o3d), "0°06′");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.5, o3d), "0°30′");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.9, o3d), "0°54′");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23, o3d), "1°14′"); // 13.8なので
-  assertStrictEquals(Angle.Degrees.toDmsString(1.234, o3d), "1°14′");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23456, o3d), "1°14′");
-  assertStrictEquals(Angle.Degrees.toDmsString(6.8563, o3d), "6°51′");
+  assertStrictEquals(Angle.degreesToDmsString(0, o3d), "0°00′");
+  assertStrictEquals(Angle.degreesToDmsString(1234567890, o3d), "90°00′");
+  assertStrictEquals(Angle.degreesToDmsString(0.1, o3d), "0°06′");
+  assertStrictEquals(Angle.degreesToDmsString(0.5, o3d), "0°30′");
+  assertStrictEquals(Angle.degreesToDmsString(0.9, o3d), "0°54′");
+  assertStrictEquals(Angle.degreesToDmsString(1.23, o3d), "1°14′"); // 13.8なので
+  assertStrictEquals(Angle.degreesToDmsString(1.234, o3d), "1°14′");
+  assertStrictEquals(Angle.degreesToDmsString(1.23456, o3d), "1°14′");
+  assertStrictEquals(Angle.degreesToDmsString(6.8563, o3d), "6°51′");
 
   const o3e = { secondFractionDigits: 1, precision: "degree" } as const;
-  assertStrictEquals(Angle.Degrees.toDmsString(0, o3e), "0°");
-  assertStrictEquals(Angle.Degrees.toDmsString(1234567890, o3e), "90°");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.1, o3e), "0°");
-  assertStrictEquals(Angle.Degrees.toDmsString(0.5, o3e), "1°"); // 0.5なので
-  assertStrictEquals(Angle.Degrees.toDmsString(0.9, o3e), "1°");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23, o3e), "1°");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.234, o3e), "1°");
-  assertStrictEquals(Angle.Degrees.toDmsString(1.23456, o3e), "1°");
-  assertStrictEquals(Angle.Degrees.toDmsString(6.8563, o3e), "7°");
+  assertStrictEquals(Angle.degreesToDmsString(0, o3e), "0°");
+  assertStrictEquals(Angle.degreesToDmsString(1234567890, o3e), "90°");
+  assertStrictEquals(Angle.degreesToDmsString(0.1, o3e), "0°");
+  assertStrictEquals(Angle.degreesToDmsString(0.5, o3e), "1°"); // 0.5なので
+  assertStrictEquals(Angle.degreesToDmsString(0.9, o3e), "1°");
+  assertStrictEquals(Angle.degreesToDmsString(1.23, o3e), "1°");
+  assertStrictEquals(Angle.degreesToDmsString(1.234, o3e), "1°");
+  assertStrictEquals(Angle.degreesToDmsString(1.23456, o3e), "1°");
+  assertStrictEquals(Angle.degreesToDmsString(6.8563, o3e), "7°");
 });
 
-Deno.test("Angle.Degrees.fromGradians()", () => {
-  assertStrictEquals(Angle.Degrees.fromGradians(0), 0);
-  assertStrictEquals(Angle.Degrees.fromGradians(400), 0);
-  assertStrictEquals(Angle.Degrees.fromGradians(200), 180);
-  assertStrictEquals(Angle.Degrees.fromGradians(100), 90);
+Deno.test("Angle.gradiansToDegrees()", () => {
+  assertStrictEquals(Angle.gradiansToDegrees(0), 0);
+  assertStrictEquals(Angle.gradiansToDegrees(400), 0);
+  assertStrictEquals(Angle.gradiansToDegrees(200), 180);
+  assertStrictEquals(Angle.gradiansToDegrees(100), 90);
   assertStrictEquals(
-    Angle.Degrees.fromGradians(400 + 100),
+    Angle.gradiansToDegrees(400 + 100),
     90,
   );
 
   assertThrows(
     () => {
-      Angle.Degrees.fromGradians("0" as unknown as number);
+      Angle.gradiansToDegrees("0" as unknown as number);
     },
     TypeError,
     undefined,
@@ -211,19 +211,19 @@ Deno.test("Angle.Degrees.fromGradians()", () => {
   );
 });
 
-Deno.test("Angle.Degrees.fromTurns()", () => {
-  assertStrictEquals(Angle.Degrees.fromTurns(0), 0);
-  assertStrictEquals(Angle.Degrees.fromTurns(1), 0);
-  assertStrictEquals(Angle.Degrees.fromTurns(0.5), 180);
-  assertStrictEquals(Angle.Degrees.fromTurns(0.25), 90);
+Deno.test("Angle.turnsToDegrees()", () => {
+  assertStrictEquals(Angle.turnsToDegrees(0), 0);
+  assertStrictEquals(Angle.turnsToDegrees(1), 0);
+  assertStrictEquals(Angle.turnsToDegrees(0.5), 180);
+  assertStrictEquals(Angle.turnsToDegrees(0.25), 90);
   assertStrictEquals(
-    Angle.Degrees.fromTurns(1 + 0.25),
+    Angle.turnsToDegrees(1 + 0.25),
     90,
   );
 
   assertThrows(
     () => {
-      Angle.Degrees.fromTurns("0" as unknown as number);
+      Angle.turnsToDegrees("0" as unknown as number);
     },
     TypeError,
     undefined,
